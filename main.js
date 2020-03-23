04aa41d8dcdfc1301b13eb6968a95c8f144edc7c
Gy <406631892@qq.com>
2020-03-23 16:38:33 +0800

    子弹做好

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ class Flyer{   //敌机类
         this.imgs = imgs
         this.speed = speed
         this.size = size
+        this.type = 2
     }
     createDOM(){
     //    创建一个DOM对象
@@ -33,11 +34,12 @@ class Flyer{   //敌机类
         gamePanel.appendChild(ele)
     }
     move(diction,speed){
-        console.log('当前飞机飞向',diction,'速度是',speed)
+        // console.log('当前飞机飞向',diction,'速度是',speed)
         let islife
         if((diction === 'w') ||(diction === 's')){
             let top = this.ele.offsetTop + speed
             islife = this.checkRange(diction,top)
+            // console.log(islife)
             if(islife)
                 this.ele.style.top = top + 'px'
         }else if((diction === 'a') ||(diction === 'd')){
@@ -51,6 +53,7 @@ class Flyer{   //敌机类
             this.destory()
         }
     }
+
     moveBefore(){
         let self = this
         this.timer = setInterval(()=>{
@@ -80,6 +83,7 @@ class Flyer{   //敌机类
         clearInterval(this.timer)
         gamePanel.removeChild(this.ele)
     }
+
 }
 
 
@@ -99,6 +103,7 @@ class MyPlane{  //定义自己的飞机
         this.imgs = imgs
         this.speed = speed
         this.size = size
+        this.type = 1
         this.MyPlaneMove = this.MyPlaneMove.bind(this)   //why?
     }
     createDOM(){
@@ -117,7 +122,6 @@ class MyPlane{  //定义自己的飞机
         gamePanel.appendChild(ele)
     }
     move(diction,speed){
-        console.log('当前飞机飞向',diction,'速度是',speed)
         if((diction === 'w') ||(diction === 's')){
             let top = this.ele.offsetTop + speed
 
@@ -178,7 +182,80 @@ class MyPlane{  //定义自己的飞机
 
 
 
+class Bullet{
+    constructor(hp,offset,imgs,size,speed) {
+        this.hp = hp
+        this.offset = offset
+        this.imgs = imgs
+        this.speed = speed
+        this.size = size
+        this.type = 3
+    }createDOM(){
+        //    创建一个DOM对象
+        let ele = document.createElement('img')
+        ele.src = this.imgs.normal
+        ele.width = this.size.h
+        ele.height = this.size.w
+        ele.style.position = 'absolute'
+        ele.style.left = this.offset.x + 'px'
+        ele.style.top = this.offset.y + 'px'
+        ele.id = 'myplane'
+        //双向数据绑定
+        ele.obj = this      //页面->内存
+        this.ele = ele      //内存-->页面
+        gamePanel.appendChild(ele)
+    }move(diction,speed){
+        // console.log('当前飞机飞向',diction,'速度是',speed)
+        let islife
+        if((diction === 'w') ||(diction === 's')){
+            let top = this.ele.offsetTop - speed
+            islife = this.checkRange(diction,top)
+            if(islife)
+                this.ele.style.top = top + 'px'
+        }else if((diction === 'a') ||(diction === 'd')){
+            let left = this.ele.offsetLeft + speed
+            islife = this.checkRange(diction,left)
+            if(islife){
+                this.ele.style.left = left + 'px'
+            }
+        }
+        if(!islife){
+            this.destory()
+        }
 
+    }
+
+    moveBefore(){
+        let self = this
+        this.timer = setInterval(()=>{
+            self.move('w',self.speed)
+        },100)
+    }
+    checkRange(diction,dist){
+        switch (diction) {
+            case 'w':
+                if(dist < gamePanel.offsetTop) return false
+                break
+            case 's':
+                if (dist > gamePanel.offsetHeight - this.size.h) return false
+                break
+            case 'a':
+                // gamePanel.offsetLeft不是0  所以手动设置为0
+                if (dist < 0) return false
+                break
+            case 'd':
+                if (dist > gamePanel.offsetWidth - this.size.w) return false
+                break
+        }
+        return  true
+    }
+    destory(){
+        //    清除定时器，删除DOM元素
+        clearInterval(this.timer)
+        gamePanel.removeChild(this.ele)
+    }
+
+}
 
 
 
@@ -202,19 +279,24 @@ GameMaster.prototype.run = function () {
     // alert('游戏成功运行')
 //    启动一个我的飞机
     setInterval(()=>{
-        let flyer = new Flyer(10,{x:random(),y:0},{normal:'./images/plane1.png',destroyed:'./images/plane1_die1.png'},{h:50,w:50},20)
+        let flyer = new Flyer(1,{x:random(),y:0},{normal:'./images/plane1.png',destroyed:'./images/plane1_die1.png'},{h:50,w:50},20)
         flyer.createDOM()
-        flyer.moveBefore()},1000)
+        flyer.moveBefore()},500)
+    setInterval(()=>{
+        let bulletPosition = {x:Myflyer.ele.offsetLeft+(Myflyer.size.w/2),y:Myflyer.ele.offsetTop+20}
+        let bullet = new Bullet(1,bulletPosition,{normal:'./images/bullet.png'},{h:5,w:20},10)
+        bullet.createDOM()
+        bullet.moveBefore()
+    },400)
     let Myflyer = new MyPlane(10,{x:200,y:400},{normal:'./images/me.png',destroyed:'./images/me_die1.png'},{h:100,w:100},20)
     Myflyer.createDOM()
     Myflyer.MyPlaneMoveBefore()
 }
-
 function gameStart(){
     new GameMaster()  //创建游戏管理实例
 }
 function random(){
     return Math.random()*(gamePanel.offsetWidth-50)
 }
-console.log()
+
 window.onload = gameStart
